c21d6f7d091b22b067d217110e1a8fbf0c00081e
Raghuvar arora <raghuvararora@gmail.com>
2022-03-29 12:44:29 +0000

    adde binding bid logic

--- auction.sol
+++ auction.sol
@@ -15,12 +15,42 @@ contract Auction{
 
     mapping(address => uint) public bids;
 
+    uint public bidIncrement;
+
 
     constructor(){
         owner=payable(msg.sender);
         auctionState=State.Running;
         startBlock=block.number;
         endBlock=startBlock+(604800/50);
-        
+        bidIncrement=100;
+        highestBindingBid=0;
+    }
+
+    modifier notOwner(){
+        require(msg.sender!=owner);
+        _;
+    }
+
+    modifier afterStart(){
+        require(block.number>=startBlock);
+        _;
+    }
+
+    modifier beforeEnd(){
+        require(block.number<=endBlock);
+        _;
+    }
+
+    function placeBid(uint bid) public payable notOwner afterStart beforeEnd{
+        require(auctionState==State.Running);
+
+    }
+
+    function placeBindingBid(uint bid) public payable notOwner afterStart beforeEnd{
+        require(auctionState==State.Running);
+        require(highestBindingBid+bidIncrement<bid, "bid not high enough");
+        highestBidder=payable(msg.sender);
+        highestBindingBid=msg.value;
     }
 }
