fb1cb7c3ece0f2a2bb169c315bd990928cc1f210
Raghuvar arora <raghuvararora@gmail.com>
2022-03-29 23:26:20 +0000

    completed auction

--- auction.sol
+++ auction.sol
@@ -14,12 +14,12 @@ contract Auction{
     address payable public highestBidder;
 
     mapping(address => uint) public bids;
-
+    
     uint public bidIncrement;
 
 
-    constructor(){
-        owner=payable(msg.sender);
+    constructor(address eoa){
+        owner=payable(eoa);
         auctionState=State.Running;
         startBlock=block.number;
         endBlock=startBlock+(604800/50);
@@ -32,25 +32,57 @@ contract Auction{
         _;
     }
 
+    modifier onlyOwner(){
+        require(msg.sender==owner);
+        _;
+    }
+
     modifier afterStart(){
         require(block.number>=startBlock);
         _;
     }
 
-    modifier beforeEnd(){
+    modifier beforeEnd() {
         require(block.number<=endBlock);
         _;
     }
 
     function placeBid(uint bid) public payable notOwner afterStart beforeEnd{
         require(auctionState==State.Running);
+        require(msg.value>bidIncrement);
 
-    }
-
-    function placeBindingBid(uint bid) public payable notOwner afterStart beforeEnd{
-        require(auctionState==State.Running);
-        require(highestBindingBid+bidIncrement<bid, "bid not high enough");
         highestBidder=payable(msg.sender);
-        highestBindingBid=msg.value;
+        highestBindingBid=bid+highestBindingBid;
+        bids[msg.sender]=highestBindingBid;
+        
     }
+
+    function cancelAuction() public onlyOwner {
+        auctionState=State.Canceled;
+    }
+
+    // complete this 
+    function finalizeAuction() public {
+        require(auctionState==State.Canceled || block.number>endBlock);
+        require(msg.sender==owner || bids[msg.sender]>0 );
+        
+        if(auctionState==State.Canceled){
+            if(bids[msg.sender]>0){
+                payable(msg.sender).transfer(bids[msg.sender]);
+                bids[msg.sender]=0;
+            }
+        }else{
+            if(msg.sender==owner ){
+                payable(highestBidder).transfer(highestBindingBid);
+                bids[highestBidder]=0;
+            }
+            else{
+                payable(msg.sender).transfer(bids[msg.sender]);
+                bids[msg.sender]=0;
+            }
+        }
+
+    }
+
+
 }
