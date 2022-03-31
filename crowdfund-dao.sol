61aff4a1845d62079425f135353576a33ca695d1
Raghuvar arora <raghuvararora@gmail.com>
2022-03-31 22:09:19 +0000

    added getBlance getRefund

--- crowdfund-dao.sol
+++ crowdfund-dao.sol
@@ -10,11 +10,46 @@ contract CrowdFunding{
     uint public deadline;
     uint public goalAmount;
     uint public raisedAmount;
+    enum State{ Success, onGoing, Failure}
+    State public currentState;
+
 
     constructor(uint _goalAmount, uint _deadline, uint _minimumContribution){
         goalAmount=_goalAmount;
-        deadline=_deadline;
+        deadline=block.timestamp+_deadline;
         manager=msg.sender;
         minimumContribution= _minimumContribution;
-    }   
+        currentState=State.onGoing;
+    }
+
+    function contribute() public payable{
+        require(msg.value>=minimumContribution);
+        require(block.timestamp<deadline);
+        if(contributors[msg.sender]==0){
+            numberOfContributers++;
+        }
+        contributors[msg.sender]=contributors[msg.sender]+msg.value;
+        raisedAmount+=msg.value;
+    }
+
+    receive() payable external{
+        contribute();
+    }
+
+    function getBalance() public view returns(uint) {
+        return raisedAmount;
+    }
+
+
+    function getRefund() public payable{
+        require(State.Failure==currentState);
+        require(contributors[msg.sender]>0);
+        require(block.timestamp>deadline);
+        address payable recipient=payable(msg.sender);
+
+        recipient.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+        
+    }
+    
 }
